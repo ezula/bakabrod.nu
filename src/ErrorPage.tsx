8a2991f8d8057bcfd93026a3b89cce7df24c8833
Mikael Svens <svensmikael@gmail.com>
2024-04-24 22:17:11 +0200

    Fix

--- src/ErrorPage.tsx
+++ src/ErrorPage.tsx
@@ -1,16 +1,9 @@
-import { useRouteError } from "react-router-dom";
-
 function ErrorPage() {
-  const error = useRouteError();
-
   return (
     <div className="container max-w-5xl mx-auto pd-4">
       <h1 className="text-6xl mt-8 mb-8">404 - Sidan finns inte</h1>
       <p className="text-2xl mt-6 mb-6">Sidan du söker finns inte, han e inte här.</p>
       <p>Prova gå till <a className="text-primary" href="https://bakabröd.nu">bakabröd.nu</a> istället.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
     </div>
   );
 }
