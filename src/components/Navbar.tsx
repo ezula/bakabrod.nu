4da6b6ae56b5b2e95abe3c122d627e370f0c2c9d
Mikael Svens <mikael@svens.tech>
2025-02-20 21:26:33 +0100

    Make hamburgermenu on mobile
    Make hamburger menu on mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,9 @@
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
   return (
     <header className="navbar bg-base-100">
       <div className="flex-1">
@@ -11,7 +14,31 @@ function Navbar() {
           bakabröd.nu
         </a>
       </div>
-      <nav className="flex-none">
+
+      {/* Hamburger button - only visible on mobile */}
+      <div className="flex-none md:hidden">
+        <button
+          className="btn btn-square btn-ghost"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block w-5 h-5 stroke-current"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
+        </button>
+      </div>
+
+      {/* Desktop menu - hidden on mobile */}
+      <nav className="flex-none hidden md:block">
         <ul className="menu menu-horizontal px-1">
           <li>
             <Link to={'contact'}>Kontakt</Link>
@@ -28,6 +55,31 @@ function Navbar() {
           </li>
         </ul>
       </nav>
+
+      {/* Mobile menu - only visible when hamburger is clicked */}
+      {isMenuOpen && (
+        <div className="md:hidden absolute top-[64px] left-0 right-0 bg-base-100 z-50">
+          <ul className="menu menu-vertical px-1 py-2">
+            <li>
+              <Link to={'contact'} onClick={() => setIsMenuOpen(false)}>
+                Kontakt
+              </Link>
+            </li>
+            <li>
+              <details>
+                <summary>Om bröd</summary>
+                <ul className="p-2 bg-base-100">
+                  <li>
+                    <Link to={'aboutbread'} onClick={() => setIsMenuOpen(false)}>
+                      Fakta om bröd
+                    </Link>
+                  </li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </div>
+      )}
     </header>
   )
 }
